fcd1576aff7f75974472e7cdbd49faa22b642e3a
Sasha-F22 <0anarchist0@gmail.com>
2025-08-29 13:43:31 +0300

    2d commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,23 @@
+
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
